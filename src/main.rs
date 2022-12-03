519c10b888e24fdd55aa8d9678ccd9196be26ba4
Chris Lefever <linuxcl4@gmail.com>
2022-12-02 21:15:20 -0500

    Add solution for day 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    part1();
+    part2();
+}
+
+fn part1() {
+    let contents = fs::read_to_string("src/day01_input.txt").expect("Could not read input file");
+
+    let mut sums = Vec::new();
+    let mut curr_sum = 0;
+
+    for num in contents.lines() {
+        if num == "" {
+            sums.push(curr_sum);
+            curr_sum = 0;
+        } else {
+            let parsed: i32 = num.parse().expect("Not a valid number");
+            curr_sum += parsed;
+        }
+    }
+
+    println!("{}", sums.iter().max().unwrap());
+}
+
+fn part2() {
+    let contents = fs::read_to_string("src/day01_input.txt").expect("Could not read input file");
+
+    let mut sums = Vec::new();
+    let mut curr_sum = 0;
+
+    for num in contents.lines() {
+        if num == "" {
+            sums.push(curr_sum);
+            curr_sum = 0;
+        } else {
+            let parsed: i32 = num.parse().expect("Not a valid number");
+            curr_sum += parsed;
+        }
+    }
+
+    sums.sort();
+    let len = sums.len();
+    let top_three = sums.iter().skip(len - 3);
+    println!("{}", top_three.sum::<i32>());
 }
