09e12b7f5f8b870c510948e126ca9abb3b33fb54
Chris Lefever <linuxcl4@gmail.com>
2022-12-03 03:35:33 -0500

    Add solution for day 2

---
+++ src/problems/day02.rs
@@ -0,0 +1,103 @@
+use std::fs;
+
+fn win_score(opponent: &str, mine: &str) -> i32 {
+    match mine {
+        "X" => match opponent {
+            "A" => 3,
+            "B" => 0,
+            "C" => 6,
+            _ => panic!("Invalid shape"),
+        },
+        "Y" => match opponent {
+            "A" => 6,
+            "B" => 3,
+            "C" => 0,
+            _ => panic!("Invalid shape"),
+        },
+        "Z" => match opponent {
+            "A" => 0,
+            "B" => 6,
+            "C" => 3,
+            _ => panic!("Invalid shape"),
+        },
+        _ => panic!("Invalid shape"),
+    }
+}
+
+fn shape_from_outcome<'a>(opponent: &str, outcome: &str) -> &'a str {
+    match outcome {
+        "X" => match opponent {
+            "A" => "Z",
+            "B" => "X",
+            "C" => "Y",
+            _ => panic!("Invalid shape"),
+        },
+        "Y" => match opponent {
+            "A" => "X",
+            "B" => "Y",
+            "C" => "Z",
+            _ => panic!("Invalid shape"),
+        },
+        "Z" => match opponent {
+            "A" => "Y",
+            "B" => "Z",
+            "C" => "X",
+            _ => panic!("Invalid shape"),
+        },
+        _ => panic!("Invalid shape"),
+    }
+}
+
+pub fn part1() {
+    let contents =
+        fs::read_to_string("src/input/day02_input.txt").expect("Could not read input file");
+
+    let mut score = 0;
+
+    for line in contents.lines() {
+        let mut iter = line.split_whitespace();
+        let opponent = iter.next().unwrap();
+        let mine = iter.next().unwrap();
+
+        let shape_score = match mine {
+            "X" => 1,
+            "Y" => 2,
+            "Z" => 3,
+            _ => panic!("Invalid shape"),
+        };
+
+        let outcome_score = win_score(opponent, mine);
+
+        score += shape_score + outcome_score;
+    }
+
+    println!("{}", score);
+}
+
+pub fn part2() {
+    let contents =
+        fs::read_to_string("src/input/day02_input.txt").expect("Could not read input file");
+
+    let mut score = 0;
+
+    for line in contents.lines() {
+        let mut iter = line.split_whitespace();
+        let opponent = iter.next().unwrap();
+        let outcome = iter.next().unwrap();
+
+        let mine = shape_from_outcome(opponent, outcome);
+
+        let shape_score = match mine {
+            "X" => 1,
+            "Y" => 2,
+            "Z" => 3,
+            _ => panic!("Invalid shape"),
+        };
+
+        let outcome_score = win_score(opponent, mine);
+
+        score += shape_score + outcome_score;
+    }
+
+    println!("{}", score);
+}
