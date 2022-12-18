5ce668fb371ec7697bab7a1642de9ef87b4b61d6
Chris Lefever <linuxcl4@gmail.com>
2022-12-18 13:11:47 -0500

    Refactoring for day 5

--- src/problems/day05.rs
+++ src/problems/day05.rs
@@ -10,75 +10,14 @@ pub fn run() {
 }
 
 fn part1(input: &[String]) -> String {
-    let mut stacks: FxHashMap<i32, Vec<String>> = FxHashMap::default();
-
-    let columns = (input[0].len() as f64 / 4.0).ceil() as i32;
-
-    for i in 0..columns {
-        stacks.insert(i + 1, Vec::new());
-    }
-
-    'outer: for line in input {
-        let mut column = 1;
-        let chars = line.chars().collect::<Vec<char>>();
-        let mut i: usize = 0;
-        while i < chars.len() {
-            let ch = chars.get(i);
-            match ch {
-                Some('[') => {
-                    let num = chars.get(i + 1).unwrap().to_string();
-                    let vec = stacks.get_mut(&column).unwrap();
-                    vec.push(num);
-                    i += 3;
-                }
-                Some(' ') => {
-                    i += 3;
-                }
-                Some('1') => {
-                    break 'outer;
-                }
-                _ => break,
-            }
-
-            column += 1;
-            i += 1;
-        }
-    }
-
-    for line in input {
-        if line.starts_with("move") {
-            let moves: Vec<&str> = line.split_whitespace().collect();
-            let amount: usize = moves[1].parse().unwrap();
-            let source: i32 = moves[3].parse().unwrap();
-            let dest: i32 = moves[5].parse().unwrap();
-
-            let to_move: Vec<_> = {
-                let source_vec = stacks.get_mut(&source).unwrap();
-                source_vec.drain(0..amount).collect()
-            };
-
-            let dest_vec = stacks.get_mut(&dest).unwrap();
-
-            for ch in to_move.iter() {
-                dest_vec.insert(0, ch.to_string());
-            }
-        }
-    }
-
-    let mut sum = Vec::new();
-
-    for col in 1..columns + 1 {
-        let ch = match stacks.get(&col).unwrap().get(0) {
-            Some(x) => x.to_string(),
-            _ => String::from(""),
-        };
-        sum.push(ch);
-    }
-
-    sum.join("")
+    stack_crates(input, false)
 }
 
 fn part2(input: &[String]) -> String {
+    stack_crates(input, true)
+}
+
+fn stack_crates(input: &[String], mult_at_once: bool) -> String {
     let mut stacks: FxHashMap<i32, Vec<String>> = FxHashMap::default();
 
     let columns = (input[0].len() as f64 / 4.0).ceil() as i32;
@@ -128,8 +67,14 @@ fn part2(input: &[String]) -> String {
 
             let dest_vec = stacks.get_mut(&dest).unwrap();
 
-            for ch in to_move.iter().rev() {
-                dest_vec.insert(0, ch.to_string());
+            if mult_at_once {
+                for ch in to_move.iter().rev() {
+                    dest_vec.insert(0, ch.to_string());
+                }
+            } else {
+                for ch in to_move.iter() {
+                    dest_vec.insert(0, ch.to_string());
+                }
             }
         }
     }
