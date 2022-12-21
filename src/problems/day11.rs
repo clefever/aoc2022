5798ef8de3c80065de1316a93747547a4622e867
Chris Lefever <linuxcl4@gmail.com>
2022-12-21 17:32:42 -0500

    Refactoring for day 11

--- src/problems/day11.rs
+++ src/problems/day11.rs
@@ -17,94 +17,15 @@ struct Monkey {
     false_path: i32,
 }
 
-fn part1(input: &String) -> i32 {
-    const MAX_ROUNDS: i32 = 20;
-
-    let mut monkeys: Vec<Monkey> = Vec::new();
-    let monkey_strings: Vec<_> = input.split("\r\n\r\n").collect();
-
-    for monkey in monkey_strings {
-        let lines: Vec<_> = monkey.lines().collect();
-
-        let items: Vec<_> = lines[1].split_whitespace().collect();
-        let items: Vec<i64> = items[2..]
-            .iter()
-            .map(|i| i.trim_end_matches(',').parse::<i64>().unwrap())
-            .collect();
-
-        let operation: Vec<_> = lines[2].split_whitespace().collect();
-        let second = operation[5].parse::<i32>().ok();
-        let operator = operation[4].chars().nth(0).unwrap();
-
-        let test = lines[3]
-            .split_whitespace()
-            .last()
-            .unwrap()
-            .parse::<i32>()
-            .unwrap();
-
-        let true_path: Vec<_> = lines[4].split_whitespace().collect();
-        let true_path = true_path.last().unwrap().parse::<i32>().unwrap();
-
-        let false_path: Vec<_> = lines[5].split_whitespace().collect();
-        let false_path = false_path.last().unwrap().parse::<i32>().unwrap();
-
-        let monkey = Monkey {
-            inspections: 0,
-            items,
-            second,
-            operator,
-            test,
-            true_path,
-            false_path,
-        };
-
-        monkeys.push(monkey);
-    }
-
-    for _ in 0..MAX_ROUNDS {
-        for i in 0..monkeys.len() {
-            let monkey3 = monkeys.get(i).unwrap();
-
-            for _ in 0..monkey3.items.len() {
-                let values = {
-                    let monkey = monkeys.get_mut(i).unwrap();
-                    let mut item = monkey.items.drain(..1).collect::<Vec<_>>()[0];
-                    monkey.inspections += 1;
-
-                    item = match monkey.operator {
-                        '+' => match monkey.second {
-                            Some(x) => item + x as i64,
-                            None => item + item,
-                        },
-                        '*' => match monkey.second {
-                            Some(x) => item * x as i64,
-                            None => item * item,
-                        },
-                        _ => panic!("Not a valid operator"),
-                    };
-
-                    (item / 3, monkey.test, monkey.true_path, monkey.false_path)
-                };
-
-                if values.0 % values.1 as i64 == 0 {
-                    let monkey2 = monkeys.get_mut(values.2 as usize).unwrap();
-                    monkey2.items.push(values.0);
-                } else {
-                    let monkey2 = monkeys.get_mut(values.3 as usize).unwrap();
-                    monkey2.items.push(values.0);
-                }
-            }
-        }
-    }
-
-    monkeys.sort_by(|a, b| a.inspections.cmp(&b.inspections));
-    monkeys.pop().unwrap().inspections * monkeys.pop().unwrap().inspections
+fn part1(input: &String) -> i64 {
+    calc_monkey_business(input, 20, true)
 }
 
 fn part2(input: &String) -> i64 {
-    const MAX_ROUNDS: i32 = 10000;
+    calc_monkey_business(input, 10000, false)
+}
 
+fn calc_monkey_business(input: &String, rounds: i32, manage_worry: bool) -> i64 {
     let mut monkeys: Vec<Monkey> = Vec::new();
     let monkey_strings: Vec<_> = input.split("\r\n\r\n").collect();
 
@@ -149,7 +70,7 @@ fn part2(input: &String) -> i64 {
 
     let modulus: i64 = monkeys.iter().map(|m| m.test as i64).product();
 
-    for _ in 0..MAX_ROUNDS {
+    for _ in 0..rounds {
         for i in 0..monkeys.len() {
             let monkey3 = monkeys.get(i).unwrap();
 
@@ -170,6 +91,10 @@ fn part2(input: &String) -> i64 {
                         },
                         _ => panic!("Not a valid operator"),
                     } % modulus;
+
+                    if manage_worry {
+                        item /= 3;
+                    }
 
                     (item, monkey.test, monkey.true_path, monkey.false_path)
                 };
